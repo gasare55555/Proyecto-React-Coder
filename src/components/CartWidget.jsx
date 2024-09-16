8ec45c12be05f9c85103366da1c2df143fc0fc44
gasare55555 <gasare55555@gmail.com>
2024-09-15 22:04:59 -0300

    octava

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,10 +1,10 @@
 import "./CartWidget.css"
-import cartPic from "../assets/cart.png"
+import cartPic from "../assets/cart.svg"
 
 function CartWidget() {
   return(
     <div id="cart-widget">
-      <img src={cartPic} alt="buy cart" width="52"/>
+      <img src={cartPic} alt="buy cart" width="48"/>
       <span className="notification">5</span>
     </div>
   )
